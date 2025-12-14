f87175f792a1bf24c32e17edf73ec51ae8f1d4d2
AFIF ZILANI <afifzilani4566@gmail.com>
2025-12-15 01:29:53 +0600

    feat: implement GET endpoint to fetch suppliers by category and remove old endpoint

---
+++ app/api/suppliers/get-all-suppliers/route.ts
@@ -0,0 +1,63 @@
+import { SupplierSupplyCategories } from "@/app/generated/prisma/enums";
+import { errorResponse, response } from "@/lib/apiResponse";
+import { throwError } from "@/lib/error";
+import prisma from "@/lib/prisma";
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+    // 1. Get the URL object from the request
+    const url = new URL(req.url);
+
+    // 2. Access search parameters using the built-in 'searchParams' property
+    try {
+        const category = url.searchParams.get(
+            "category"
+        ) as keyof typeof SupplierSupplyCategories;
+        if (!Object.values(SupplierSupplyCategories).includes(category)) {
+            throwError({
+                message: "Invalid supplier category",
+                statusCode: 400,
+            });
+        }
+        const suppliers = await prisma.suppliers.findMany({
+            where: {
+                type: {
+                    hasSome: [SupplierSupplyCategories[category]],
+                },
+            },
+            select: {
+                id: true,
+                profile: {
+                    select: {
+                        name: true,
+                    },
+                },
+                company: true,
+            },
+        });
+        console.log(suppliers);
+
+        if (!suppliers.length) {
+            throwError({
+                message: "No suppliers Data found",
+                statusCode: 404,
+            });
+        }
+
+        let data: { id: string; name: string; company?: string }[] = [];
+        suppliers.map((val) =>
+            data.push({
+                id: val.id,
+                name: val.profile.name,
+                company: val.company ? val.company : undefined,
+            })
+        );
+
+        return response({
+            message: "successfully fetch supplers data",
+            data,
+        });
+    } catch (error) {
+        return errorResponse(error);
+    }
+}
